8093b09270b60617a8f4a9fe4258da3a8d04291a
kjmkagaya <jm.kagaya@gmail.com>
2018-06-19 12:04:20 +0900

    Done

---
+++ JavaKadai013/src/JavaKadai014.java
@@ -0,0 +1,10 @@
+
+public class JavaKadai014 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "加賀屋　ジャンメデル";
+		System.out.println(name + "\t\t" + "カガヤ　ジャンめでる");
+	}
+
+}
