649d17ea75fbe67f3d06fe880089dbada0168183
kjmkagaya <jm.kagaya@gmail.com>
2018-06-20 11:00:13 +0900

    Done
    Done

---
+++ JavaKadai051/src/JavaKadai051.java
@@ -0,0 +1,35 @@
+
+import java.math.*;
+import java.util.*;
+public class JavaKadai051 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		 
+		while(true) {
+			System.out.print("®”’l‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢[[„");
+		  int num1 = sc.nextInt();
+		  if(num1 %7 != 0 && num1 < 0) {
+			  System.out.println("³‚Ì”‚Å‚V‚Ì”{”‚Å‚ ‚è‚Ü‚¹‚ñ");
+			    
+		  }
+		  if (num1 == 0) {
+			  System.out.println("0 ‚Å‚·");
+		  }
+		  else if (num1 <0) {
+			  System.out.println("•‰‚Ì”‚Å‚·");
+		  }
+		 
+		 
+		}
+			
+		
+		
+		
+	}
+
+}
