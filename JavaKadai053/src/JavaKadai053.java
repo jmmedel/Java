6b269bc9922ab462bde3607c357651db21071c84
kjmkagaya <jm.kagaya@gmail.com>
2018-06-20 11:00:57 +0900

    Done
    Done

---
+++ JavaKadai053/src/JavaKadai053.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class JavaKadai053 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("1つの整数値を入力してくださいーー＞");
+		int num = sc.nextInt();
+		System.out.println("２つの整数値を入力してくださいーー＞");
+		int num2 = sc.nextInt();
+		
+		
+		if (num > num2)
+		{
+			System.out.println("1つ目のデータの方が大きいです");
+		}
+		else 
+			System.out.println("２つ目のデータの方が大きいです");
+		
+		
+	}
+
+}
