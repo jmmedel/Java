f0935655be3acee3982d43d57d570ecd6cbac329
kjmkagaya <jm.kagaya@gmail.com>
2018-06-20 10:49:50 +0900

    Done
    Done

---
+++ JavaKadai022/src/JavaKadai022.java
@@ -0,0 +1,10 @@
+
+public class JavaKadai022 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 float pi =  (float) 3.1415;
+		 System.out.print("‰~Žü—¦‚Í"+pi+"‚Å‚·");
+	}
+
+}
