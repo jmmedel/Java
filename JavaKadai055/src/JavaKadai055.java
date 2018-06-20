7cea5725365f85243efedb3a73c99288e5691559
kjmkagaya <jm.kagaya@gmail.com>
2018-06-20 11:01:40 +0900

    Done
    Done

---
+++ JavaKadai055/src/JavaKadai055.java
@@ -0,0 +1,57 @@
+import java.util.*;
+public class JavaKadai055 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Œ‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢[[„");
+		int months = sc.nextInt();
+		
+		switch (months) {
+		case 1:
+			System.out.print("“~‚Å‚·");
+			break;
+		case 2:
+			System.out.print("“~‚Å‚·");
+			break;
+		case 12:
+			System.out.print("“~‚Å‚·");
+			break;
+			
+		case 3:
+			System.out.print("t‚Å‚·");
+			break;
+		case 4:
+			System.out.print("t‚Å‚·");
+			break;
+		case 5:
+			System.out.print("t‚Å‚·");
+			break;
+		case 6:
+			System.out.print("‰Ä‚Å‚·");
+			break;
+		case 7:
+			System.out.print("‰Ä‚Å‚·");
+			break;
+		case 8:
+			System.out.print("‰Ä‚Å‚·");
+			break;
+		case 9:
+			System.out.print("H‚Å‚·");
+			break;
+		case 10:
+			System.out.print("H‚Å‚·");
+			break;
+		case 11:
+			System.out.print("H‚Å‚·");
+			break;
+			
+		default:
+			System.out.println("ƒG[‚ç");
+			break;
+		}
+	
+
+	}
+
+}
