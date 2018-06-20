a4d37170e2146b77b6d41b7bdcf8d01187a10ab8
kjmkagaya <jm.kagaya@gmail.com>
2018-06-20 10:56:05 +0900

    Done
    Done

---
+++ JavaKadai046/src/JavaKadai046.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.math.*;
+public class JavaKadai046 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("®”’l‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢[[„");
+		int num1 = sc.nextInt();
+		System.out.printf("Data1:" + num1 +  "Data2:" + Math.abs(num1));
+		
+	}
+
+}
