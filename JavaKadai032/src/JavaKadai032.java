ef8323552ba6e2aad2b890bd5309177c4d0e3a7a
kjmkagaya <jm.kagaya@gmail.com>
2018-06-20 10:51:03 +0900

    Done
    Done

---
+++ JavaKadai032/src/JavaKadai032.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class JavaKadai032 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.print("生年月日の年を西暦で入力してくださいーー＞");
+		int year = sc.nextInt();
+		System.out.print("月を入力してくださいーー＞");
+		int months = sc.nextInt();
+		System.out.print("日を入力してくださいーー＞");
+		int day = sc.nextInt();
+		System.out.printf("生年月日は　%d/%d/%d",year,months,day);
+		
+		
+		
+	}
+
+}
