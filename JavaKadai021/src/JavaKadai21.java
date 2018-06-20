377c47eade6464899b980f4c6e1e013075607d41
kjmkagaya <jm.kagaya@gmail.com>
2018-06-20 10:49:11 +0900

    Done
    Done

---
+++ JavaKadai021/src/JavaKadai21.java
@@ -0,0 +1,12 @@
+
+public class JavaKadai21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int  data = 12345;
+		System.out.printf("12345は8進数で%o　です\n",data);
+		System.out.printf("12345は10進数で%d です\n",data);
+		System.out.printf("12345は16進数で%x です\n",data);
+	}
+
+}
