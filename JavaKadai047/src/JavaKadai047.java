edbe1021cd7e9ac9e1147947c56fdc37a9a199ec
kjmkagaya <jm.kagaya@gmail.com>
2018-06-20 10:59:52 +0900

    Done
    Done

---
+++ JavaKadai047/src/JavaKadai047.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class JavaKadai047 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// TODO Auto-generated method stub
+				Scanner sc= new Scanner(System.in);
+				System.out.println("1つ目の整数値を入力してくださいーー＞");
+				double num1 = sc.nextDouble();
+				System.out.println("2つ目の整数値を入力してくださいーー＞");
+				double num2 = sc.nextDouble();
+				
+				System.out.println(num1 + "+" + num2 + "=" + (num1 + num2));
+				System.out.println(num1 + "-" + num2 + "=" + (num1 - num2));
+				System.out.println(num1 + "*" + num2 + "=" + (num1 * num2));
+				System.out.println(num1 + "/" + num2 + "=" + (num1 / num2));
+				System.out.println(num1 + "%" + num2 + "=" + (num1 % num2));
+			}
+	
+	}
+
+
