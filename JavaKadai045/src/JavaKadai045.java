020006d410b3966dcb4087300cec401b823e4d74
kjmkagaya <jm.kagaya@gmail.com>
2018-06-20 10:55:43 +0900

    Done
    done

---
+++ JavaKadai045/src/JavaKadai045.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class JavaKadai045 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("®”’l‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢[[„");
+		int num1 = sc.nextInt();
+		System.out.println(num1 + "x" + "10" + "=" + (num1 * 10));
+		
+	}
+
+}
