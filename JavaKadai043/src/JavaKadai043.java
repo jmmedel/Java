c33704b6b0b0a34141b2b460a5b7f55061be894c
kjmkagaya <jm.kagaya@gmail.com>
2018-06-22 11:21:15 +0900

    Done
    Doine

--- JavaKadai043/src/JavaKadai043.java
+++ JavaKadai043/src/JavaKadai043.java
@@ -1,9 +1,16 @@
+// Done
 
+import java.util.Scanner;
 public class JavaKadai043 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("®”’l‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢[[„");
+		int num1 = sc.nextInt();
+		System.out.print((num1 /1000)* 1000);
 	}
 
 }
