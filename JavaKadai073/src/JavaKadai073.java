cf902421e5e6fbfb333f800d8240630a2ae6dd9f
kjmkagaya <jm.kagaya@gmail.com>
2018-06-22 10:29:07 +0900

    Done
    Done

---
+++ JavaKadai073/src/JavaKadai073.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class JavaKadai073 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int total = 0;
+		while(true)
+		{
+			
+			System.out.println("data-enter");
+			int num1 = sc.nextInt();
+			total = total + num1;
+			System.out.println( "Total = "+ total);
+		}
+		
+	}
+
+}
