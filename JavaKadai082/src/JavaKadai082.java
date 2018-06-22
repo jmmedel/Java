21655e0ffa72a11791041839eb8fedf31fd032fb
kjmkagaya <jm.kagaya@gmail.com>
2018-06-22 10:32:21 +0900

    Done
    Done

---
+++ JavaKadai082/src/JavaKadai082.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class JavaKadai082 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner stdin = new Scanner(System.in);
+		System.out.print("１つのデータを入力してくださいーー＞");
+		double a = stdin.nextDouble();
+		System.out.print("2つのデータを入力くださいーー＞");
+		double b = stdin.nextDouble();
+		dispMax(a,b);
+		
+		
+	}
+
+	private static void dispMax(double a, double b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
