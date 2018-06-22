04559657f06d3384552846e9540cd8872ac90a06
kjmkagaya <jm.kagaya@gmail.com>
2018-06-22 11:23:13 +0900

    Done
    Done

--- JavaKadai044/src/JavaKadai044.java
+++ JavaKadai044/src/JavaKadai044.java
@@ -1,9 +1,10 @@
-
+import java.util.*;
 public class JavaKadai044 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Scanner sc = new Scanner(System.in);
 		
 		
 	}
