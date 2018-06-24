cc9d38a9f866c6f09b4ace5ccb6bf957799dfd57
kjmkagaya <jm.kagaya@gmail.com>
2018-06-25 02:49:08 +0900

    Done

--- JavaKadai066/src/JavaKadai066.java
+++ JavaKadai066/src/JavaKadai066.java
@@ -1,9 +1,23 @@
+// Done
 
+import java.util.*;
 public class JavaKadai066 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		double sum = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter you score ");
+		while(true) {
+			double data = sc.nextDouble();
+			if(data > 0 && data <100 ) {
+				sum += data;
+				System.out.println(sum);
+			}
+			else
+				System.out.println("Please enter a passitiven number");
+			
+		}
 	}
 
 }
