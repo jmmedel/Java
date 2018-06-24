9eaf9ac7d8ddbf7c20b1f182997cd56060620537
kjmkagaya <jm.kagaya@gmail.com>
2018-06-25 02:40:42 +0900

    Done

--- JavaKadai065/src/JavaKadai065.java
+++ JavaKadai065/src/JavaKadai065.java
@@ -1,9 +1,24 @@
-
+import java.util.*;
 public class JavaKadai065 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int counter = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+	    System.out.print("Enter a number");
+		double data = sc.nextDouble();
+		
+		if(data<0) {
+			counter++;
+			System.out.println("the data enter -->"  + counter);
+		}
+			
+		}
+		
+		
 	}
 
 }
