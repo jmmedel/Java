a89abcf9f46ef567437d1307ace2211a9358b039
kjmkagaya <jm.kagaya@gmail.com>
2018-06-25 13:50:27 +0900

    Done

--- JavaKadai068/src/JavaKadai068.java
+++ JavaKadai068/src/JavaKadai068.java
@@ -1,3 +1,4 @@
+// Done
 import java.util.Scanner;
 
 public class JavaKadai068 {
@@ -17,21 +18,60 @@ public class JavaKadai068 {
 			clear = 0.4;
 			cloudy = 0.4;
 			rainy = 0.2;
-			System.out.printf("明日の晴れの確率は  %.2f 曇りの確率は %.2f　雨の確率は %.2f",clear,cloudy,rainy);
+			double clear2;
+			double cloudy2;
+			double rainy2;
+			
+			for (int i = 1; i < 8; i++) {
+				 clear2 = clear *0.4 + cloudy *  0.3 + rainy * 0.3;
+				 cloudy2 =  clear * 0.4 + cloudy * 0.4 + rainy * 0.5;
+				 rainy2 = clear * 0.2 + cloudy * 0.3 + rainy * 0.2;
+				 
+						 
+				System.out.printf(i+"日後の晴れの確率は  %.2f 曇りの確率は %.2f　雨の確率は %.2f \n",clear2,cloudy2,rainy2);
+				
+			}
+			
 			break;
 		
 		case 1:
 			clear = 0.3;
 			cloudy = 0.4;
 			rainy = 0.3;
-			System.out.printf("明日の晴れの確率は  %.2f 曇りの確率は %.2f　雨の確率は %.2f",clear,cloudy,rainy);
+			double clear3;
+			double cloudy3;
+			double rainy3;
+			
+			for (int i = 1; i < 8; i++) {
+				 clear3 = clear *0.4 + cloudy *  0.3 + rainy * 0.3;
+				 cloudy3 =  clear * 0.4 + cloudy * 0.4 + rainy * 0.5;
+				 rainy3 = clear * 0.2 + cloudy * 0.3 + rainy * 0.2;
+				 
+						 
+				System.out.printf(i+"日後の晴れの確率は  %.2f 曇りの確率は %.2f　雨の確率は %.2f \n",clear3,cloudy3,rainy3);
+				
+			}
+			
 			break;
 		
 		case 2:
 			clear = 0.3;
 			cloudy = 0.5;
 			rainy = 0.2;
-			System.out.printf("明日の晴れの確率は  %.2f 曇りの確率は %.2f　雨の確率は %.2f",clear,cloudy,rainy);
+			double clear4;
+			double cloudy4;
+			double rainy4;
+			
+			for (int i = 1; i < 8; i++) {
+				 clear4 = clear *0.4 + cloudy *  0.3 + rainy * 0.3;
+				 cloudy4 =  clear * 0.4 + cloudy * 0.4 + rainy * 0.5;
+				 rainy4 = clear * 0.2 + cloudy * 0.3 + rainy * 0.2;
+				 
+						 
+				System.out.printf(i+"日後の晴れの確率は  %.2f 曇りの確率は %.2f　雨の確率は %.2f \n",clear4,cloudy4,rainy4);
+				
+			}
+			
 			break;
 			
 		default:
