bd8a19b5e5f192c107475ad856dd7b19520ec258
kjmkagaya <jm.kagaya@gmail.com>
2018-06-25 13:31:57 +0900

    Done

--- JavaKadai067/src/JavaKadai067.java
+++ JavaKadai067/src/JavaKadai067.java
@@ -1,9 +1,33 @@
+// Done
 
+import java.util.*;
+import java.math.*;
 public class JavaKadai067 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		Scanner sc = new Scanner(System.in);
+		double num;
+		int length ;
+		System.out.print("正の整数値を入力してくださいーー＞");
+		while(true) {
+	     num = sc.nextDouble();
+	     if(num > 0) {
+	     length = (int)(Math.log10(num) + 1);
+	     System.out.print(length);
+	     break;	 
+	     }
+	     else {
+	    	 System.out.print("正の整数値を入力してくださいーー＞");
+	     }
+	    	 
+		}
+		
+		
+				
+		
 	}
 
 }
