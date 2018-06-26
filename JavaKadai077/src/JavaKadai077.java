e060e152dc5114a93d4df35063ab4913121d6649
kjmkagaya <jm.kagaya@gmail.com>
2018-06-26 11:13:15 +0900

    Done
    Done

--- JavaKadai077/src/JavaKadai077.java
+++ JavaKadai077/src/JavaKadai077.java
@@ -7,56 +7,48 @@ public class JavaKadai077 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int countA = 0;
-		int countB = 0;
-		int countC = 0;
-		int countD = 0;
-		int countErr = 0;
-		for (int i = 1; i < 81; i++) {
+		double countA = 0;
+		double countB = 0;
+		double countC = 0;
+		double countD = 0;
+		double countErr = 0;
+		for (int i = 1; i < 11; i++) {
 			
 			int answer = 0;
 			System.out.println(i + "–â‚Ì‰ð“š[[„");
 			answer = sc.nextInt();
-			if(answer > -1) {
-				switch (answer) {
-				case 0:
+			
+				
+				if(answer == 0)
+				{
 					countA++;
-					System.out.println("A = "+(countA / 80) * 100);
-					
-					
-					break;
-				case 1:
-					++countB;
-					
-					System.out.println("B = "+(countB /80)* 100 );
-					
-					break;
-				case 2:
-					++countC;
-					
-					System.out.println("C =" +(countC / 80) * 100 );
-					
-					break;
-				case 3:
-					++countD;
-					
-					System.out.println("D = "+(countD /80) * 100 );
-					
-					break;
-				default:
-					++countErr;
-					System.out.println("Error" + (countErr / 80) * 100 );
-					break;
-					
+					System.out.println("A = "+countA);
+				}
+				else if (answer == 1) {
+					countB++;
+					System.out.println("B = "+countB);
+				}
+				else if (answer == 2) {
+					countC++;
+					System.out.println("C = "+countC);
 				}
 				
-				
-			}
-			System.out.println("Error" + countA );
-			System.out.println("Error" + countB / 80 * 100 );
-			System.out.println("Error" + countC );
-			System.out.println("Error" + countD );
-			System.out.println("Error" + countErr );
+				else if (answer == 3) {
+					countD++;
+					System.out.println("D = "+countD);
+				}
+				else {
+					countErr++;
+				}
+			
+				if(i == 10) {
+					System.out.printf("%2f",(countA/10)*100);
+					System.out.printf("%2f",(countB/10)*100);
+					System.out.printf("%2f",(countC/10)*100);
+					System.out.printf("%2f",(countD/10)*100);
+				}
+			
+			
 		}
 		
 	}
