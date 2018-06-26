5fdc75a8506ffcb921b15881840abf9c91e3b767
kjmkagaya <jm.kagaya@gmail.com>
2018-06-26 11:23:06 +0900

    Done
    Done

--- JavaKadai077/src/JavaKadai077.java
+++ JavaKadai077/src/JavaKadai077.java
@@ -12,7 +12,7 @@ public class JavaKadai077 {
 		double countC = 0;
 		double countD = 0;
 		double countErr = 0;
-		for (int i = 1; i < 11; i++) {
+		for (int i = 1; i < 81; i++) {
 			
 			int answer = 0;
 			System.out.println(i + "問の解答ーー＞");
@@ -22,30 +22,30 @@ public class JavaKadai077 {
 				if(answer == 0)
 				{
 					countA++;
-					System.out.println("A = "+countA);
+					
 				}
 				else if (answer == 1) {
 					countB++;
-					System.out.println("B = "+countB);
+					
 				}
 				else if (answer == 2) {
 					countC++;
-					System.out.println("C = "+countC);
+					
 				}
 				
 				else if (answer == 3) {
 					countD++;
-					System.out.println("D = "+countD);
+					
 				}
 				else {
 					countErr++;
 				}
 			
-				if(i == 10) {
-					System.out.printf("%2f",(countA/10)*100);
-					System.out.printf("%2f",(countB/10)*100);
-					System.out.printf("%2f",(countC/10)*100);
-					System.out.printf("%2f",(countD/10)*100);
+				if(i == 80) {
+					System.out.printf("ア　＝ %.2f \n",(countA/80)*100);
+					System.out.printf("イ　＝%.2f \n",(countB/80)*100);
+					System.out.printf("ウ　＝%.2f \n",(countC/80)*100);
+					System.out.printf("エ　＝%.2f \n",(countD/80)*100);
 				}
 			
 			
