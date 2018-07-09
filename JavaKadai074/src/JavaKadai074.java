9f2053c6f03906dff492b7d5cf59a850ea031985
kjmkagaya <jm.kagaya@gmail.com>
2018-07-09 13:23:15 +0900

    Done
    Done

--- JavaKadai074/src/JavaKadai074.java
+++ JavaKadai074/src/JavaKadai074.java
@@ -1,9 +1,34 @@
+// Done
+
+import java.util.Scanner;
 
 public class JavaKadai074 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int[][] Array = new int [5][10];
+		for (int i = 0; i < 5; i++) {
+			 
+			for (int j = 0; j < 10; j++) {
+				System.out.print(i + "s" + j +"—á‚Ì’lF");
+				Array[i][j] = sc.nextInt();
+				System.out.println();
+			}
+		}
+		
+		for (int i = 0; i < 5; i++) {
+			
+			for (int j = 0; j < 10; j++) {
+				
+				System.out.print(Array[i][j]);
+			}
+			System.out.println();
+		}
+		
+		
 	}
 
 }
