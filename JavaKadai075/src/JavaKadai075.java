da89ec5afd286febdb90969f7abc798927f61435
kjmkagaya <jm.kagaya@gmail.com>
2018-08-18 06:15:10 +0900

    Done
    Done

--- JavaKadai075/src/JavaKadai075.java
+++ JavaKadai075/src/JavaKadai075.java
@@ -5,6 +5,13 @@ public class JavaKadai075 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+		get_score();
+	
+		
+	}
+	
+	public static void get_score() {
 		Scanner sc = new Scanner(System.in);
 		int total = 0;
 		int get_number;
@@ -13,24 +20,32 @@ public class JavaKadai075 {
 			for (int j = 1; j < 4; j++) {
 				System.out.println("番号"+ i + "学生の科目" + j + "の得点を入力してくださいーー＞");
 				get_number = sc.nextInt();
-				while(get_number > -2 & get_number < 100) {
+				if(get_number > -2 & get_number < 101) {
 				   
-				   if(get_number == -1) {
+					
+				    if(get_number == -1) {
 					   
 					   break;
 				   }
 				   else if (get_number == -2) {
 					 
-					   System.exit(0);
+					  break;
 				   }
+				   else {
+					   total = total + get_number;
+					   break;
+				   }
+					   
+				   
 				   
 				}
 				
 			}
-			System.out.println("総合得点は"+ total);
+			if(i == 5) {
+				System.out.println("総合得点は"+ total);
+				}
 			
 		}
-		
 	}
 
 }
