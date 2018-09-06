408487992cdfcc4e72b10d1758ff8aaea5d460d1
kjmkagaya <jm.kagaya@gmail.com>
2018-09-06 10:28:33 +0900

    done
    done

--- JavaKadai083/src/JavaKadai083.java
+++ JavaKadai083/src/JavaKadai083.java
@@ -1,9 +1,25 @@
-
+import java.util.*;
 public class JavaKadai083 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int[] ay = {42,66,47,28,59,37};
+		int[] by;
+		for (int i = 0; i < ay.length; i++) {
+			System.out.printf("%5d",ay[i]);
+		}
+			by = arrayCopy(ay);
+			ay[2] = 600;
+			System.out.printf("%n コピー元データ ");
+		
 	}
-
+	
+	
+	public static int[] arrayCopy(int[] data) {
+		
+		return data;
+		
+	}
+	
 }
