785ef4b7346bf6e25544b46b34bcd040bbd7ea04
kjmkagaya <jm.kagaya@gmail.com>
2018-09-06 11:39:35 +0900

    done
    done

--- JavaKadai084/src/JavaKadai084.java
+++ JavaKadai084/src/JavaKadai084.java
@@ -1,9 +1,25 @@
-
+import java.util.Arrays;
 public class JavaKadai084 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int [] data = {32,56,31,56,47,56,15};
+		for (int i = 0; i < data.length; i++) {
+			System.out.printf("%5d",data[i]);
+			
+		}
+		System.out.printf("%n");
+		int m = getMedia(data);
+		System.out.printf("’†‰›’l‚Í%d‚Å‚·\n", m);
+		
+	}
+	
+	public static int  getMedia(int[] data) {
+		 Arrays.sort(data);
+		int middle = data.length/2;
+	
+		return data[middle];
 	}
 
 }
