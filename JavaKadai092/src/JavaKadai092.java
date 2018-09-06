bd0a3bc3683f279b92f31f3f6e4d64a4e2f9435d
kjmkagaya <jm.kagaya@gmail.com>
2018-09-06 14:22:04 +0900

    done
    done

--- JavaKadai092/src/JavaKadai092.java
+++ JavaKadai092/src/JavaKadai092.java
@@ -6,8 +6,26 @@ public class JavaKadai092 {
 		
 		
 		int[] Data = {23,45,21,67,95,43,76,19};
-		
+		System.out.println("”z—ñ‚Ì•½‹Ï‚Í" + getAvg(Data) + "‚Å‚·");
+		System.out.println("”z—ñ‚Ìæ“ª‚©‚ç4ŒÂ‚Ì•½‹Ï‚Í "
+				 + getAvg(Data,4)+ "‚Å‚·");
+		System.out.println("”z—ñ‚Ìæ“ª‚©‚ç15ŒÂ‚Ì•½‹Ï‚Í"
+				+ getAvg(Data,15) + "‚Å‚·");
+	}
 
+	private static String getAvg(int[] data, int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static double getAvg(int[] data) {
+		// TODO Auto-generated method stub
+		double sum = 0;
+		for (int i = 0; i < data.length; i++) {
+			sum += data[i];
+		}
+		sum += sum/data.length;
+		return sum;
 	}
 
 }
