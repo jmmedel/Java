2bbcabcd88a0b21dc274e9a996c63054ee9f0ddc
kjmkagaya <jm.kagaya@gmail.com>
2018-09-06 13:33:23 +0900

    done
    done

--- JavaKadai091/src/JavaKadai091.java
+++ JavaKadai091/src/JavaKadai091.java
@@ -1,9 +1,29 @@
-
+import java.util.*;
 public class JavaKadai091 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	 
+		int Data1 = 50;
+		int Data2 = 20;
+		
+		int[] Data3 = {42,67,45,21,30,83,55};
+		int Min;
+		Min = getMin(Data1,Data2);
+		System.out.println("");
+		Min = getMin(Data3);
+		System.out.println("配列のデータの最小値は"+ Min + "です");
+		
+	}
 
+	private static int getMin(int[] data3) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private static int getMin(int data1, int data2) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
