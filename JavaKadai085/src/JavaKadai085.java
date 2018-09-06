5206e20829919000e10ab4711e3bac9f65f66173
kjmkagaya <jm.kagaya@gmail.com>
2018-09-06 12:00:47 +0900

    done
    done

--- JavaKadai085/src/JavaKadai085.java
+++ JavaKadai085/src/JavaKadai085.java
@@ -1,9 +1,22 @@
-
+import java.util.*;
 public class JavaKadai085 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int year,nyear;
+		String str;
+		Calendar cal = Calendar.getInstance();
+		nyear = cal.get(Calendar.YEAR);
+		Scanner stdin = new Scanner(System.in);
+		System.out.printf("¼—ï”N‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢[[„");
+		year = stdin.nextInt();
+		str = toWareki(year);
+	}
 
+	public static String toWareki(int year) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
