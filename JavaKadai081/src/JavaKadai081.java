9afb2a0d938c702872a8de6fa822b0cbae339828
kjmkagaya <jm.kagaya@gmail.com>
2018-09-06 09:40:07 +0900

    done
    done

--- JavaKadai081/src/JavaKadai081.java
+++ JavaKadai081/src/JavaKadai081.java
@@ -5,7 +5,7 @@ public class JavaKadai081 {
 		// TODO Auto-generated method stub
 		int a = 35 , b = 40 , c = 72, d = 49, e = 53;
 		double v = getAvg(a,b,c,d,e);
-		System.out.println(v);
+		System.out.printf("%d,%d,%d,%d,%d ‚Ì•½‹Ï‚Í%f‚Å‚·",a,b,c,d,e,v);
 	}
 
 	private static double getAvg(int a, int b, int c, int d, int e) {
