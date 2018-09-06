9ea309d6dc4b811e4db39f7d50b5b135f5484554
kjmkagaya <jm.kagaya@gmail.com>
2018-09-06 14:48:03 +0900

    done
    done

--- JavaKadai093/src/JavaKadai093.java
+++ JavaKadai093/src/JavaKadai093.java
@@ -1,9 +1,19 @@
 
+import java.util.*;
 public class JavaKadai093 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		double Data1;
+		double Data2;
+		Scanner stdin = new Scanner(System.in);
+		System.out.printf("1個目のデーターー＞");
+		Data1 = stdin.nextDouble();
+		System.out.println("2個目のデーターー＞");
+		Data2 = stdin.nextDouble();
+		Ans = getArea(Data1);
+		System.out.printf("1辺%fの正方形の面積%fです\n", args)
 	}
 
 }
