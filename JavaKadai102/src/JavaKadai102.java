e86f37f1018a69f30b6b1aa74c06d5110e75c8b9
kjmkagaya <jm.kagaya@gmail.com>
2018-09-07 11:04:49 +0900

    Done
    Done

--- JavaKadai102/src/JavaKadai102.java
+++ JavaKadai102/src/JavaKadai102.java
@@ -3,23 +3,75 @@ public class JavaKadai102 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-			if( args[0]== "+".toString()) {
-				System.out.println("Hello");
-				//System.out.println(Integer.parseInt(args[1])  + Integer.parseInt(args[2]));
-			}//else if(args[0] == "-") {
-				//System.out.println(Integer.parseInt(args[1])  - Integer.parseInt(args[2]));
-			//}else if(args[0] == "/") {
-			//	System.out.println(Integer.parseInt(args[1])  / Integer.parseInt(args[2]));
-			//} else if(args[0] == "*") {
-			//	System.out.println(Integer.parseInt(args[1])  * Integer.parseInt(args[2]));
-			
-			
-			
-			
-			
+		for (int i = 0; i < 1; i++) {
+			if(args[i] == "+") {
+				Addition(args);
+			}
 		}
+		
+		
+		//Subtraction(args);
+		//Division(args);
+		//Multiplication(args);
+		
 	}
+	
+	public static void Addition(String[] args) {
+		 int sum;
+	   	 int i;
+	   	 sum = Integer.parseInt(args[1]);          // Initialize the running sum
+	    
+	   	 for ( i = 2; i < args.length; i++ )
+	   	 {
+	   	    sum = sum + Integer.parseInt(args[i]);   // Add args[i] to running sum  
+	 
+	   	 }
+	    
+	   	 System.out.println( sum );
+	}
+	
+	
+	public static void Subtraction(String[] args) {
+		 int ans;
+	   	 int i;
+	   	 ans = Integer.parseInt(args[1]);          // Initialize the running sum
+	    
+	   	 for ( i = 2; i < args.length; i++ )
+	   	 {
+	   	    ans = ans - Integer.parseInt(args[i]);   // Add args[i] to running sum  
+	 
+	   	 }
+	    
+	   	 System.out.println( ans );
+	}
+	
+	public static void Division(String[] args) {
+		 int ans;
+	   	 int i;
+	   	 ans = Integer.parseInt(args[1]);          // Initialize the running sum
+	    
+	   	 for ( i = 2; i < args.length; i++ )
+	   	 {
+	   	    ans = ans / Integer.parseInt(args[i]);   // Add args[i] to running sum  
+	 
+	   	 }
+	    
+	   	 System.out.println( ans );
+	}
+	
+	public static void Multiplication(String[] args) {
+		 int ans;
+	   	 int i;
+	   	 ans = Integer.parseInt(args[1]);          // Initialize the running sum
+	    
+	   	 for ( i = 2; i < args.length; i++ )
+	   	 {
+	   	    ans = ans * Integer.parseInt(args[i]);   // Add args[i] to running sum  
+	 
+	   	 }
+	    
+	   	 System.out.println( ans );
+	}
+	
 
 }
