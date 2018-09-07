01ff26083b412fc50fe2ed7f29c734f5e6cdeffc
kjmkagaya <jm.kagaya@gmail.com>
2018-09-07 10:46:28 +0900

    Done
    Dome

--- JavaKadai102/src/JavaKadai102.java
+++ JavaKadai102/src/JavaKadai102.java
@@ -4,16 +4,17 @@ public class JavaKadai102 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for (String word : args) {
-			if(word == "+") {
-				System.out.println(Integer.parseInt(args[1])  + Integer.parseInt(args[2]));
-			}else if(word == "-") {
-				System.out.println(Integer.parseInt(args[1])  - Integer.parseInt(args[2]));
-			}else if(word == "/") {
-				System.out.println(Integer.parseInt(args[1])  / Integer.parseInt(args[2]));
-			} else if(word == "*") {
-				System.out.println(Integer.parseInt(args[1])  * Integer.parseInt(args[2]));
-			}
+		
+			if( args[0]== "+".toString()) {
+				System.out.println("Hello");
+				//System.out.println(Integer.parseInt(args[1])  + Integer.parseInt(args[2]));
+			}//else if(args[0] == "-") {
+				//System.out.println(Integer.parseInt(args[1])  - Integer.parseInt(args[2]));
+			//}else if(args[0] == "/") {
+			//	System.out.println(Integer.parseInt(args[1])  / Integer.parseInt(args[2]));
+			//} else if(args[0] == "*") {
+			//	System.out.println(Integer.parseInt(args[1])  * Integer.parseInt(args[2]));
+			
 			
 			
 			
