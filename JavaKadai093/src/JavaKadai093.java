ea5fc94ea94c59507df0c8ec230cfe0cd62f318d
kjmkagaya <jm.kagaya@gmail.com>
2018-09-07 09:55:20 +0900

    Done
    done

--- JavaKadai093/src/JavaKadai093.java
+++ JavaKadai093/src/JavaKadai093.java
@@ -7,13 +7,31 @@ public class JavaKadai093 {
 		
 		double Data1;
 		double Data2;
+		double Ans;
 		Scanner stdin = new Scanner(System.in);
 		System.out.printf("1個目のデーターー＞");
 		Data1 = stdin.nextDouble();
 		System.out.println("2個目のデーターー＞");
 		Data2 = stdin.nextDouble();
-		Ans = getArea(Data1);
-		System.out.printf("1辺%fの正方形の面積%fです\n", args)
+		 Ans = getArea(Data1);
+		System.out.printf("1辺%fの正方形の面積%fです\n", Data1,Ans);
+		Ans = getArea(Data1,Data2);
+		System.out.printf("幅高さ%f,%fの長方形の面積は%fです\n", Data1,Data2,Ans);
+		
+	}
+
+	private static double getArea(double data1, double data2) {
+		// TODO Auto-generated method stub
+		double ans = data1 * data2;
+		
+		return ans;
+	}
+
+	private static double getArea(double data1) {
+		// TODO Auto-generated method stub
+		double ans = data1*data1;
+		
+		return ans;
 	}
 
 }
