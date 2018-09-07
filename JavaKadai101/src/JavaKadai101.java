c433d8aaddec56aabbb6c4fd4821253f087b2ac6
kjmkagaya <jm.kagaya@gmail.com>
2018-09-07 10:06:54 +0900

    Done
    done

--- JavaKadai101/src/JavaKadai101.java
+++ JavaKadai101/src/JavaKadai101.java
@@ -3,7 +3,9 @@ public class JavaKadai101 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		for (String word : args) {
+			System.out.println(word);
+		}
 	}
 
 }
