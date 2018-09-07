bc362c45425c51ec1ff626841e35114d8c2fe527
kjmkagaya <jm.kagaya@gmail.com>
2018-09-07 10:21:43 +0900

    Done
    done

--- JavaKadai101/src/JavaKadai101.java
+++ JavaKadai101/src/JavaKadai101.java
@@ -3,8 +3,9 @@ public class JavaKadai101 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i = 0 ;
 		for (String word : args) {
-			System.out.println(word);
+			System.out.println("args[" + i++ + "]" + "=" + word);
 		}
 	}
 
