d8c8896e2da7ef28b67804744084e63867ca2b6c
kjmkagaya <jm.kagaya@gmail.com>
2018-11-19 15:55:51 +0900

    Done
    Done

---
+++ GUIRei01/src/GuiRei01.java
@@ -0,0 +1,42 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class GuiRei01 {
+
+	private JFrame frmh;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GuiRei01 window = new GuiRei01();
+					window.frmh.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GuiRei01() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmh = new JFrame();
+		frmh.setTitle("0H01007\u52A0\u8CC0\u5C4B\u3000\u30B8\u30E3\u30F3\u30E1\u30C7\u30EB");
+		frmh.setBounds(100, 100, 450, 300);
+		frmh.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
