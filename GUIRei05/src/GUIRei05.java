bd50a9f5f3b3f84839a51013115bae6390b1bded
kjmkagaya <jm.kagaya@gmail.com>
2018-11-26 13:32:11 +0900

    Done
    Done

--- GUIRei05/src/GUIRei05.java
+++ GUIRei05/src/GUIRei05.java
@@ -3,10 +3,24 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.JRadioButton;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.border.TitledBorder;
+import java.awt.Color;
 
 public class GUIRei05 {
 
 	private JFrame frmh;
+	private JRadioButton ARadioButton;
+	private JRadioButton BRadioButton;
+	private JRadioButton ORadioButton;
+	private JRadioButton ABRadioButton;
+	private JLabel DispLabel;
+	private JButton ReadButton;
+	private JButton EndButton;
 
 	/**
 	 * Launch the application.
@@ -40,8 +54,40 @@ public class GUIRei05 {
 		frmh.setBounds(100, 100, 450, 300);
 		frmh.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JPanel panel = new JPanel();
-		frmh.getContentPane().add(panel, BorderLayout.NORTH);
+		JPanel BloodPanel = new JPanel();
+		BloodPanel.setBorder(new TitledBorder(null, "\u8840\u6DB2\u578B", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		frmh.getContentPane().add(BloodPanel, BorderLayout.NORTH);
+		BloodPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		ARadioButton = new JRadioButton("A\u578B");
+		ARadioButton.setSelected(true);
+		BloodPanel.add(ARadioButton);
+		
+		ORadioButton = new JRadioButton("O\u578B");
+		BloodPanel.add(ORadioButton);
+		
+		ABRadioButton = new JRadioButton("AB\u578B");
+		BloodPanel.add(ABRadioButton);
+		
+		BRadioButton = new JRadioButton("B\u578B");
+		BloodPanel.add(BRadioButton);
+		
+		JPanel ButtonPanel = new JPanel();
+		frmh.getContentPane().add(ButtonPanel, BorderLayout.SOUTH);
+		ButtonPanel.setLayout(new BoxLayout(ButtonPanel, BoxLayout.X_AXIS));
+		
+		ReadButton = new JButton("\u5360\u3044");
+		ReadButton.setToolTipText("\u30AF\u30EA\u30C3\u30AF\u3059\u308B\u3068\u5360\u3044");
+		ButtonPanel.add(ReadButton);
+		
+		EndButton = new JButton("\u7D42\u4E86");
+		EndButton.setToolTipText("\u30AF\u30EA\u30C3\u30AF\u3059\u308B\u3068\u7D42\u4E86\u3057\u307E\u3059");
+		ButtonPanel.add(EndButton);
+		
+		DispLabel = new JLabel("\u3053\u3053\u306B\u5360\u3044\u7D50\u679C\u3092\u8868\u793A\u3057\u307E\u3059");
+		DispLabel.setOpaque(true);
+		DispLabel.setBackground(Color.YELLOW);
+		frmh.getContentPane().add(DispLabel, BorderLayout.WEST);
 	}
 
 }
