22f8e243d0057139556759ef154823527f2f62f9
kjmkagaya <jm.kagaya@gmail.com>
2018-11-28 11:27:52 +0900

    Done
    Done

--- GUIRei11/src/GUIRei11.java
+++ GUIRei11/src/GUIRei11.java
@@ -1,10 +1,11 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class GUIRei11 {
 
-	private JFrame frame;
+	private JFrame frmh;
 
 	/**
 	 * Launch the application.
@@ -14,7 +15,7 @@ public class GUIRei11 {
 			public void run() {
 				try {
 					GUIRei11 window = new GUIRei11();
-					window.frame.setVisible(true);
+					window.frmh.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -33,9 +34,15 @@ public class GUIRei11 {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmh = new JFrame();
+		frmh.setTitle("0H01007 \u52A0\u8CC0\u5C4B\u30B8\u30E3\u30F3\u30E1\u30C7\u30EB");
+		frmh.setBounds(100, 100, 296, 207);
+		frmh.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmh.getContentPane().setLayout(null);
+		
+		JLabel DispLabel = new JLabel("");
+		DispLabel.setBounds(75, 30, 120, 110);
+		frmh.getContentPane().add(DispLabel);
 	}
 
 }
