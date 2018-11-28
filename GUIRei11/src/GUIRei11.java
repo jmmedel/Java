d5e53e95213b9ba445303e5cb2382a1577e5fd4c
kjmkagaya <jm.kagaya@gmail.com>
2018-11-28 11:44:50 +0900

    Done
    Done

--- GUIRei11/src/GUIRei11.java
+++ GUIRei11/src/GUIRei11.java
@@ -1,12 +1,14 @@
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class GUIRei11 {
 
 	private JFrame frmh;
-
+	private ImageIcon img=new ImageIcon("C:\\Users\\kagaya john\\Desktop\\Git_Java\\GUIRei11\\src\\yuru05.jpg");
 	/**
 	 * Launch the application.
 	 */
@@ -36,13 +38,22 @@ public class GUIRei11 {
 	private void initialize() {
 		frmh = new JFrame();
 		frmh.setTitle("0H01007 \u52A0\u8CC0\u5C4B\u30B8\u30E3\u30F3\u30E1\u30C7\u30EB");
-		frmh.setBounds(100, 100, 296, 207);
+		frmh.setBounds(100, 100, 393, 242);
 		frmh.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmh.getContentPane().setLayout(null);
 		
 		JLabel DispLabel = new JLabel("");
-		DispLabel.setBounds(75, 30, 120, 110);
+		DispLabel.setBounds(69, 25, 126, 115);
 		frmh.getContentPane().add(DispLabel);
+		DispLabel = new JLabel(img);
+		DispLabel.setBounds(50,25,151,115);
+		frmh.getContentPane().add(DispLabel);
+		
+		JLabel lblNewLabel = new JLabel("\u30E6\u30FC\u30B9\u30B1\u30FC\u30B9\u56F3\u306E\u30A2\u30AF\u30BF\u30FC");
+		lblNewLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+		lblNewLabel.setBounds(221, 63, 126, 13);
+		frmh.getContentPane().add(lblNewLabel);
+		
 	}
 
 }
