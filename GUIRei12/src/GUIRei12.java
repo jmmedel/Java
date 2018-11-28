a2499c32d3c0bed06451c3e1c8c92d9eac6e37bb
kjmkagaya <jm.kagaya@gmail.com>
2018-11-28 12:34:25 +0900

    Done
    Done

--- GUIRei12/src/GUIRei12.java
+++ GUIRei12/src/GUIRei12.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 
 public class GUIRei12 {
 
-	private JFrame frame;
+	private JFrame frmh;
 
 	/**
 	 * Launch the application.
@@ -14,7 +14,7 @@ public class GUIRei12 {
 			public void run() {
 				try {
 					GUIRei12 window = new GUIRei12();
-					window.frame.setVisible(true);
+					window.frmh.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -33,9 +33,10 @@ public class GUIRei12 {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmh = new JFrame();
+		frmh.setTitle("0h01007 \u52A0\u8CC0\u5C4B\u3000\u30B8\u30E3\u30F3\u30E1\u30C7\u30EB");
+		frmh.setBounds(100, 100, 450, 300);
+		frmh.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
