ac37cb13040a75aa7c9b15ccb6ea7a80b7ecdd58
kjmkagaya <jm.kagaya@gmail.com>
2018-11-30 11:07:23 +0900

    Done
    done

--- GUIKadai07/src/GUIKadai07.java
+++ GUIKadai07/src/GUIKadai07.java
@@ -4,10 +4,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class GUIKadai07 extends JFrame {
 
 	private JPanel contentPane;
+	private JLabel DispLabel;
 
 	/**
 	 * Launch the application.
@@ -29,12 +34,30 @@ public class GUIKadai07 extends JFrame {
 	 * Create the frame.
 	 */
 	public GUIKadai07() {
+		setTitle("0H01007 \u52A0\u8CC0\u5C4B\u3000\u30B8\u30E3\u30F3\u30E1\u30C7\u30EB(\u6C0F\u540D\u8868\u793A)");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 255, 203);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		DispLabel = new JLabel("\u6C0F\u540D\uFF1A");
+		DispLabel.setBounds(23, 39, 83, 30);
+		contentPane.add(DispLabel);
+		
+		JButton btnNewButton = new JButton("\u6C0F\u540D\u5165\u529B");
+		btnNewButton.setBounds(23, 122, 91, 21);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("\u7D42\u4E86");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		btnNewButton_1.setBounds(138, 122, 91, 21);
+		contentPane.add(btnNewButton_1);
 	}
 
 }
